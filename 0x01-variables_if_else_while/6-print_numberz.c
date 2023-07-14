0fd07cebd2c4a2efbd71975e42009b4bea68e4b9
Tegibar1983 <tbtf1991@gmail.com>
2023-07-14 06:45:26 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -C programm eEntry point
+ * Return: Always zero(0) on sucess
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
