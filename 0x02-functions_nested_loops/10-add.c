588f93293f4a752f0f342f9523c19210aa8ea5aa
Tegibar1983 <tbtf1991@gmail.com>
2023-07-18 08:32:37 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,43 +2,19 @@
 #include <stdlib.h>
 
 /**
- *  * print_times_table - times table for n
+ *  * add - add two numbers
  *   *
- *    * @n: value of table to be printed starting from 0
+ *    * @a: first number
  *     *
- *      * Return: void
+ *      * @b: second number
+ *       *
+ *        * Return: sum of two numbers
  */
 
-void print_times_table(int n)
+int add(int a, int b)
 {
-	int num, multi, prod;
+int sum;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-			for (multi = 1; multi <= n; multi++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * multi;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+sum = a + b;
+return (sum);
 }
