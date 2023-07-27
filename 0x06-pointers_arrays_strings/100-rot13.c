11eadac1e04f89325ebd3ed9a37a949a8d19e0f0
Tegibar1983 <tbtf1991@gmail.com>
2023-07-26 17:04:19 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  * rot13 - Rot13 encoding
+ *   * Return: Pointer to array
+ *    * @s: String
+ */
+char *rot13(char *s)
+{
+	char half1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+
+	char half2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i = 0, j;
+
+	while (s[i] != 0)
+	{
+		char c = s[i];
+
+		for (j = 0; j < 52; j++)
+		{
+			if (c == half1[j])
+			{
+				s[i] = half2[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
