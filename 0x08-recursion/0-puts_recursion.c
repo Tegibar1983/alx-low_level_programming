d9a79caf04006db3aab61b1af7ebee9933aec3e7
Tegibar1983 <tbtf1991@gmail.com>
2023-08-01 23:26:21 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,13 @@
  * _puts_recursion - function to print string
  * Return - 0
  */
-
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
-{
 	_putchar('\n');
-}
 else
-_putchar(*s);
+{
+	_putchar(*s);
 _puts_recursion(s + 1);
+}
 }
