9c13b3cd74276c23f26444234b774472ceed4897
Tegibar1983 <tbtf1991@gmail.com>
2023-08-10 04:08:48 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - function that returns pointer
+ * @b: input argument size
+ * Return: pointer or NULL on failor
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *n;
+if (n == NULL)
+exit(98);
+
+n = (int *)malloc(b);
+
+return (n);
+
+}
