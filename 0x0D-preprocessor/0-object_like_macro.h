7e1581fba617c2961a375765cbe0b20691f40f10
Tegibar1983 <tbtf1991@gmail.com>
2023-08-14 06:14:25 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef SIZE
+#define SIZE 1024
+
+#endif/*macro named SIZE & value is 1024*/
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ *  * main - program entry pount
+ *   *
+ *    * Return: zero onsucess.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
