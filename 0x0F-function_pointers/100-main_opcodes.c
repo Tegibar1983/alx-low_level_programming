36aa44b94e85d21790996019661c43334b868847
Tegibar1983 <tbtf1991@gmail.com>
2023-08-16 19:43:56 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-  /**
-   *    * main - prints opcodes
-   *       * @argc: arguments count
-   *          * @argv: array arrgument
-   *                * Return: 0
-   */
+/**
+ * main - Print opcodes
+ * @argc:  arguments counts  supply to the program
+ * @arg argument value
+ * Return: Zero
+ */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+int bytes, index;
+int (*address)(int, char **) = main;
+unsigned char opcode;
 
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-
-	bytes = atoi(argv[1]);
-
-
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-
-	arr = (char *)main;
-
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
 }
 
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index < bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+
+address++;
+}
+
+printf("\n");
+
+return (0);
+}
